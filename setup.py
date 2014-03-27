72db099f86ef587a95898edd8a8f56397c53e2a3
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2014-03-27 11:44:05 +0100

    Flask as a required install.

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='settei-example',
-    version='1.0',
+    name = 'settei-example',
+    version = '1.0',
     packages = find_packages(),
+    install_requires = [
+        'flask'
+    ],
     entry_points = {
         'settings_application': [
             'default = settings.application.default:generate_config',
